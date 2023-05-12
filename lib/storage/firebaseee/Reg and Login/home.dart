b1fe4cdd628062062a9606bc32866fecdcf2a8ff
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-05-12 12:15:37 +0530

    commit

--- lib/storage/firebaseee/Reg and Login/home.dart
+++ lib/storage/firebaseee/Reg and Login/home.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:samplefeb/storage/firebaseee/Reg%20and%20Login/fire_authentication.dart';
+import 'package:samplefeb/storage/firebaseee/Reg%20and%20Login/login.dart';
 
 class Homef extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Welcome"),
+            ElevatedButton(onPressed: () {
+              FireHelper().signOut().then((v) {
+                Get.to(LoginPage());
+              });
+            }, child: Text("SignOut"))
+          ],
+        ),
+      ),
+    );
   }
 }
