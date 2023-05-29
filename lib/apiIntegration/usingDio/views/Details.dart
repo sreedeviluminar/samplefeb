631a474a3ad2289a48bf0635eb10438f4bbe6f7c
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-05-29 14:49:13 +0530

    commit

---
+++ lib/apiIntegration/usingDio/views/Details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  int index;
+
+  DetailsPage({required this.index,Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/apiIntegration/usingDio/views/Home.dart
+++ lib/apiIntegration/usingDio/views/Home.dart
@@ -2,8 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:lottie/lottie.dart';
+import 'package:path/path.dart';
 import 'package:samplefeb/apiIntegration/usingDio/controller/controller.dart';
 import 'package:samplefeb/apiIntegration/usingDio/utils/colors.dart';
+import 'package:samplefeb/apiIntegration/usingDio/utils/constSnack.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+
+import 'Details.dart';
 
 void main() {
   runApp(GetMaterialApp(
@@ -20,19 +27,16 @@ class HomePost extends StatelessWidget {
       backgroundColor: MyColors.bgcolor,
       appBar: buildAppBar(),
       floatingActionButton: Obx(() =>
-       controller.isInternetConnected.value
-          ? buildFAB()
-          : Container()),
-
+          controller.isInternetConnected.value ? buildFAB() : Container()),
       body: Obx(
-            () =>
-            SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: controller.isInternetConnected.value ?
-                       controller.isLoading.value ? buildLoading() : buildMainBody()
-                    : buildNoInternetConnection(context)
-            ),
+        () => SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: controller.isInternetConnected.value
+                ? controller.isLoading.value
+                    ? buildLoading()
+                    : buildMainBody()
+                : buildNoInternetConnection(context)),
       ),
     );
   }
@@ -41,7 +45,7 @@ class HomePost extends StatelessWidget {
     return AppBar(
       backgroundColor: MyColors.prcolor,
       centerTitle: true,
-      title: Text("Get Api from Dio"),
+      title: const Text("Get Api from Dio"),
     );
   }
 
@@ -58,9 +62,90 @@ class HomePost extends StatelessWidget {
             : FontAwesomeIcons.arrowDown));
   }
 
-  buildLoading() {}
+  Center buildLoading() {
+    return Center(
+      child: SizedBox(
+        height: 150,
+        width: 150,
+        child: Lottie.network(
+            "https://assets8.lottiefiles.com/packages/lf20_t9gkkhz4.json"),
+      ),
+    );
+  }
 
-  buildMainBody() {}
+  Center buildNoInternetConnection(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 200,
+            width: 200,
+            child: Lottie.network(
+                "https://assets4.lottiefiles.com/packages/lf20_tasn1wnv.json"),
+          ),
+          MaterialButton(
+            onPressed: () => onTapButton(context),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            color: MyColors.prcolor,
+            child: const Text(
+              "Try Again",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
-  buildNoInternetConnection(BuildContext context) {}
+  void onTapButton(BuildContext context) async {
+    if (await InternetConnectionChecker().hasConnection == true) {
+      controller.getPosts();
+    } else {
+      showCustomSnackbar(context);
+    }
+  }
+
+  RefreshIndicator buildMainBody() {
+    return RefreshIndicator(
+        onRefresh: (){
+          return controller.getPosts();
+        },
+        child: ScrollablePositionedList.builder(
+            itemScrollController: controller.itemController,
+            physics: BouncingScrollPhysics(),
+            itemCount: controller.posts.length,
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  Get.to(DetailsPage(index: index),
+                      transition: Transition.leftToRightWithFade);
+                },
+                child: Card(
+                  child: ListTile(
+                    leading: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.greenAccent,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Text(controller.posts[index].id.toString()),
+                    ),
+                    title: Text(
+                      controller.posts[index].title,
+                      style: TextStyle(color: Colors.lightGreenAccent),
+                    ),
+                    subtitle: Text(
+                      controller.posts[index].body,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+              );
+            }));
+  }
 }
